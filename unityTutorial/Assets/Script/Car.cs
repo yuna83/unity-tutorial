afa3409b3f7f50352451e59c7e2642916de61898
Yuna <b1a4coco@gmail.com>
2023-07-27 17:37:51 +0900

    (2023.7.27) 변수의 선언

--- unityTutorial/Assets/Script/Car.cs
+++ unityTutorial/Assets/Script/Car.cs
@@ -1,10 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
+
 public class Car : MonoBehaviour
-{ //클래스 시작 위치
+{
+    #region
 
-  //위에서부터 아래로 프로그램 실행
+    char character = 'A';
 
-} //클래스가 끝나는 위치
+    int integer = 100;
+
+    float pi = 3.14f;
+
+    string wname = "sonata";
+
+   
+
+    private void Start()
+    {
+        //변수는 프로그램이 실행되는 동안 값을 바꿀 수 있으며
+        //원래 저장되어 있는 값은 새로 저장되는 값에 의해 지워진다
+
+        character = 'B';
+        integer = 5000;
+        pi = 6.65f;
+        wname = "ava";
+
+        Debug.Log("character 변수의 값:"+ character);
+        Debug.Log("int 변수의 값:" + integer);
+        Debug.Log("pi 변수의 값:" + pi);
+        Debug.Log("name 변수의 값:" + wname);
+    }
+
+     #endregion
+}
