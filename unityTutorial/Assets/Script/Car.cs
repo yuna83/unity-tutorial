5784cd3f176859afeb0bde52823e94eceb211a34
Yuna <b1a4coco@gmail.com>
2023-07-28 17:38:45 +0900

    (2023.7.28) 함수

--- unityTutorial/Assets/Script/Car.cs
+++ unityTutorial/Assets/Script/Car.cs
@@ -10,11 +10,12 @@ public class Car : MonoBehaviour
 
     char character = 'A';
 
-    int integer = 100;
+    public int integer = 100;
 
-    float pi = 3.14f;
+    private float pi = 3.14f;
 
     string wname = "sonata";
+    //접근지정자 선언 x시, private 자동 지정
 
    
 
@@ -24,7 +25,6 @@ public class Car : MonoBehaviour
         //원래 저장되어 있는 값은 새로 저장되는 값에 의해 지워진다
 
         character = 'B';
-        integer = 5000;
         pi = 6.65f;
         wname = "ava";
 
@@ -34,5 +34,29 @@ public class Car : MonoBehaviour
         Debug.Log("name 변수의 값:" + wname);
     }
 
-     #endregion
+    #endregion
+
+    #region 함수
+    //하나의 특정한 목적의 작업 수행 코드의 집합
+    private void Stop()
+    {
+        Debug.Log("정지");
+    }
+    private void Update()
+    {
+        //조건문: 조건에 따라 동작 실행 명령문
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Stop();
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("핸들 조정");
+        }
+        else
+        {
+            Debug.Log("시동 꺼짐");
+        }
+    }
+    #endregion
 }
